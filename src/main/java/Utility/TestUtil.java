4dbcea490f22b09f646b71c3f88254e597b95ac7
Priyanka <priyanka.pawar@netsutra.com>
2021-04-28 12:56:27 +0530

    modified test util package

--- src/main/java/Utility/TestUtil.java
+++ src/main/java/Utility/TestUtil.java
@@ -4,7 +4,7 @@ import com.TestBase.TestBase;
 
 public class TestUtil extends TestBase {
 	
-	public static long PAGE_LOAD_TIMEOUT=50;
+	public static long PAGE_LOAD_TIMEOUT=10;
 	public static long IMPLICIT_WAIT=50;
 	
 
