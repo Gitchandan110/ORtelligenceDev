410f7194ef3c3cdf4314cd47f74e935b873fbd02
Priyanka Pawar <priyanka.pawar@netsutra.com>
2021-04-28 13:03:50 +0530

    Time change to 10 seconds in utility

--- src/main/java/Utility/TestUtil.java
+++ src/main/java/Utility/TestUtil.java
@@ -5,7 +5,7 @@ import com.TestBase.TestBase;
 public class TestUtil extends TestBase {
 	
 	public static long PAGE_LOAD_TIMEOUT=10;
-	public static long IMPLICIT_WAIT=50;
+	public static long IMPLICIT_WAIT=10;
 	
 
 }
