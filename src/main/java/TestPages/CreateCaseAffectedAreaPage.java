ddab3d5e6fef585fee280bb1e189771dd847b963
Priyanka Pawar <priyanka.pawar@netsutra.com>
2021-05-10 12:40:04 +0530

    Commented file errors

--- src/main/java/TestPages/CreateCaseAffectedAreaPage.java
+++ src/main/java/TestPages/CreateCaseAffectedAreaPage.java
@@ -8,177 +8,177 @@ import com.TestBase.TestBase;
 
 public class CreateCaseAffectedAreaPage extends TestBase
 {
-//	We have to use Page Factory-Object Repository
-//	Here we will segregate all locators using @FindBy annotation
-//	Speciality = Trauma .................................................................................
-//	Clavicle
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Proximal Humerus
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Humerus
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Distal Humerus
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Proximal Forearm
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Forearm
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Wrist
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Pelvis/Acetabulum
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Hip
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Femur
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Distal Femur
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Patella
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Proximal Tibia
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Tibia
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Distal Tibia 
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Ankle
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Foot
-	@FindBy(xpath="")
-	WebElement ;
-
-//	Speciality =  Arthroplasty .................................................................................
-	
-//	Shoulder
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Elbow
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Hand
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Hip
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Knee
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Ankle
-	@FindBy(xpath="")
-	WebElement ;
-	
-	
-	
-//	Speciality = Spine .................................................................................
-//	Cervical
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Thoracic
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Lumbar
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Occiput
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Sacrum
-	@FindBy(xpath="")
-	WebElement ;
-	
-
-	
-//	Speciality = Sports .................................................................................
-//	Shoulder
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Humerus
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Elbow
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Forearm
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Wrist/Hand
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Hip
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Femur
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Knee
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Tibia
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Ankle
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	Foot
-	@FindBy(xpath="")
-	WebElement ;
-	
-//	...........................................................................................................
-//	Back
-	@FindBy(xpath="")
-//	WebElement ;	
+////	We have to use Page Factory-Object Repository
+////	Here we will segregate all locators using @FindBy annotation
+////	Speciality = Trauma .................................................................................
+////	Clavicle
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Proximal Humerus
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Humerus
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Distal Humerus
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Proximal Forearm
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Forearm
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Wrist
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Pelvis/Acetabulum
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Hip
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Femur
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Distal Femur
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Patella
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Proximal Tibia
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Tibia
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Distal Tibia 
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Ankle
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Foot
+//	@FindBy(xpath="")
+//	WebElement ;
+//
+////	Speciality =  Arthroplasty .................................................................................
+//	
+////	Shoulder
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Elbow
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Hand
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Hip
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Knee
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Ankle
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+//	
+//	
+////	Speciality = Spine .................................................................................
+////	Cervical
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Thoracic
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Lumbar
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Occiput
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Sacrum
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+//
+//	
+////	Speciality = Sports .................................................................................
+////	Shoulder
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Humerus
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Elbow
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Forearm
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Wrist/Hand
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Hip
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Femur
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Knee
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Tibia
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Ankle
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	Foot
+//	@FindBy(xpath="")
+//	WebElement ;
+//	
+////	...........................................................................................................
+////	Back
+//	@FindBy(xpath="")
+////	WebElement ;	
 	
 	
 	
